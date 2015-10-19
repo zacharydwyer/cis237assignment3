9bd18aca62731729358b5ba6e123a01a1a68f998
zacharydwyer <zdwyer4035@mail.kvcc.edu>
2015-10-19 04:05:39 -0400

    Forgot to add untracked files

---
+++ cis237assignment3/Label.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    class Label : DrawableElement
+    {
+        // Labels have the same properties as DrawableElements so there's really no need to do anything further here
+        public Label(EDisplaySetting displaySetting, string label)
+            : base(displaySetting, label) { }
+
+        // Again, the same as DrawableElement
+        public override int Width
+        {
+            get
+            {
+                return this.Label.Length;
+            }
+        }
+
+        // Labels are not selectable
+        public override bool IsSelectable
+        {
+            get { return false; }
+        }
+    }
+}
---
+++ cis237assignment3/ProtocolDroid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment3
+{
+    class ProtocolDroid : Droid
+    {
+        // Each known language adds 150 credits to the cost
+        public const int COST_PER_LANGUAGE = 150;
+
+        // Number of languages this droid knows.
+        public int NumberOfLanguages { get; set; }
+
+        // Constructor; calls parent class Droid first, then assigns numberOfLanguages to itself
+        public ProtocolDroid(Material material, DroidModel model, DroidColor color, int numberOfLanguages)
+            : base(material, model, color)
+        {
+            this.NumberOfLanguages = numberOfLanguages;
+            CalculateTotalCost();
+        }
+
+        // Overridden CalculateTotalCost method, which now takes into consideration the number of languages
+        public override void CalculateTotalCost()
+        {
+            base.CalculateTotalCost();                                  // Call Droid's CalculateTotalCost (which sets the TotalCost to the BaseCost of the Droid)
+
+            // Add the number of languages * cost per language value to the total cost
+            TotalCost += (this.NumberOfLanguages * COST_PER_LANGUAGE);
+        }
+
+
+    }
+}
